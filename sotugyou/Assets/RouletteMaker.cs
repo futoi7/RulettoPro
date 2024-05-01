7cde5c6a78b2081736b9d363366d3a914022c72a
matsuurataketo <g2127063@st.t-kougei.ac.jp>
2024-05-01 16:25:20 +0900

    ｑ

--- sotugyou/Assets/RouletteMaker.cs
+++ sotugyou/Assets/RouletteMaker.cs
@@ -23,11 +23,14 @@ public class RouletteMaker : MonoBehaviour
 
     //RouletteControllerアッタッチされているオブジェクト
     [SerializeField] private RouletteController rController;
+
+    public float ratePerRoulette;
+    public float rotatePerRoulette;
     private void Start()
     {
         //画像の分割数を求めている
-        float ratePerRoulette = 1 / (float)choices.Count;
-        float rotatePerRoulette = 360 / (float)(choices.Count);
+        ratePerRoulette = 1 / (float)choices.Count;
+        rotatePerRoulette = 360 / (float)(choices.Count);
 
 
         for (int i = 0; i < choices.Count; i++)
@@ -42,10 +45,10 @@ public class RouletteMaker : MonoBehaviour
             //書き換えたいプレファブの子供を探し出し書き換えている
             obj.GetComponentInChildren<TextMeshProUGUI>().text = choices[(choices.Count - 1 - i)];
             //同じようにやろうとしたが失敗
-            childImage.sprite = Images[(choices.Count - 1 - i)];
+            //childImage.sprite = Images[(choices.Count - 1 - i)];
             //子オブジェクトの回転をさせています
             obj.transform.GetChild(0).transform.rotation = Quaternion.Euler(0, 0, ((rotatePerRoulette / 2) + rotatePerRoulette * i));
-            obj.transform.GetChild(1).transform.rotation = Quaternion.Euler(0, 0, ((rotatePerRoulette / 2) + rotatePerRoulette * i));
+            //obj.transform.GetChild(1).transform.rotation = Quaternion.Euler(0, 0, ((rotatePerRoulette / 2) + rotatePerRoulette * i));
         }
         rController.rMaker = this;
         rController.rotatePerRoulette = rotatePerRoulette;
